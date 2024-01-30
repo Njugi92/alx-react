12ad0f717f7ff1162d7c4a6b4abffdcad6c72d26
Njugi92 <kenkaruma.kk@gmail.com>
2024-01-30 03:18:02 +0300

    using webpack with config file

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,18 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+const updateCounter = _.debounce(function () {
+    let count = parseInt($('#count').text()) || 0;
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+}, 1000);
+
+$(document).ready(function () {
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<button id="startBtn">Click here to get started</button>');
+    $('body').append('<p id="count"></p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
+
+    $('#startBtn').on('click', updateCounter);
+});
