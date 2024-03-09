5fa27d8e08381264e3b4b7e24feddc0427fecb85
Njugi92 <kenkaruma.kk@gmail.com>
2024-03-09 14:07:46 +0000

    Connect notifications: update the test suites

--- 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionCreators.test.js
+++ 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionCreators.test.js
@@ -1,22 +1,34 @@
-import { MARK_AS_READ, SET_TYPE_FILTER, NotificationTypeFilters } from './notificationActionTypes';
-import { markAsAread, setNotificationFilter } from './notificationActionCreators';
-  
-describe('actions', () => {
-  it('markAsRead', () => {
-    const data = {
+import {
+  MARK_AS_READ,
+  SET_TYPE_FILTER,
+  NotificationTypeFilters,
+} from "./notificationActionTypes";
+
+import {
+  markAsAread,
+  setNotificationFilter,
+} from "./notificationActionCreators";
+
+describe("action creators tests", function () {
+  it("returns correct action for markAsRead", function () {
+    const expectedReturn = {
       type: MARK_AS_READ,
       index: 1,
     };
+
     const result = markAsAread(1);
-    expect(result).toEqual(data);
+
+    expect(result).toEqual(expectedReturn);
   });
-  
-  it('setNotificationFilter', () => {
-    const data = {
+
+  it("returns correct action for setNotificationFilter", function () {
+    const expectedReturn = {
       type: SET_TYPE_FILTER,
-      filter: 'DEFAULT',
+      filter: "DEFAULT",
     };
+
     const result = setNotificationFilter(NotificationTypeFilters.DEFAULT);
-    expect(result).toEqual(data);
+
+    expect(result).toEqual(expectedReturn);
   });
 });
